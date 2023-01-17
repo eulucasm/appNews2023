d1544f370f2783ab712723af00e648988b0f20f3
lucas.mmarques.ext <lucas.mmarques.ext@panonline.com.br>
2023-01-16 21:16:36 -0300

    configuraçao Retrofit

---
+++ app/src/main/java/com/study/lucao/appnews/util/Constants.kt
@@ -0,0 +1,9 @@
+package com.study.lucao.appnews.util
+
+class Constants {
+
+    companion object{
+        const val API_KEY = "3420c7a024a84470a573e0fbf398e4fc"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
